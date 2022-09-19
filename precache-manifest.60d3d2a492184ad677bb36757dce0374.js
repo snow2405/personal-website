af3b6019e9b71e762faf2d15c324f6950b87e4d0
snow2405 <hey@lionsix.ch>
2022-09-19 13:30:51 +0200

    commit 17

---
+++ precache-manifest.60d3d2a492184ad677bb36757dce0374.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b58192d7f3bcd84d84815f9bb576981",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "571cd1403e7195c46776",
+    "url": "/home/static/css/2.24860e4a.chunk.css"
+  },
+  {
+    "revision": "5763fa50ca36bead380c",
+    "url": "/home/static/css/main.11b79a65.chunk.css"
+  },
+  {
+    "revision": "571cd1403e7195c46776",
+    "url": "/home/static/js/2.e21c754a.chunk.js"
+  },
+  {
+    "revision": "b2c477522e6e47a24340",
+    "url": "/home/static/js/3.ba268bdf.chunk.js"
+  },
+  {
+    "revision": "5763fa50ca36bead380c",
+    "url": "/home/static/js/main.b66151b5.chunk.js"
+  },
+  {
+    "revision": "cb007dd5c67acbf5b533",
+    "url": "/home/static/js/runtime~main.81d3b128.js"
+  },
+  {
+    "revision": "ca88398ad918a1468e95c3f53a918399",
+    "url": "/home/static/media/paper.ca88398a.pdf"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/home/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/home/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/home/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/home/static/media/slick.f97e3bbf.svg"
+  }
+]);
